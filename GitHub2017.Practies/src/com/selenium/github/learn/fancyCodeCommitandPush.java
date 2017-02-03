f437d4f772a5d2dfa063923623f3de95afd5ffa0
Nurzat <nurzat.nijat@aol.com>
2017-02-03 14:46:19 -0800

    Project first committed

---
+++ GitHub2017.Practies/src/com/selenium/github/learn/fancyCodeCommitandPush.java
@@ -0,0 +1,16 @@
+package com.selenium.github.learn;
+
+import java.util.HashMap;
+
+public class fancyCodeCommitandPush {
+
+	 public static void main(String[] args) {
+	        
+	        HashMap<String, String>  obj1 = new HashMap<String, String>();
+	        obj1.put("Alive is","Awesome");
+	        obj1.put("Love","Yourself");        
+	        System.out.println(obj1);        
+	    }    
+	 
+	
+}
